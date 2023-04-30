e6734318dde4fba971064dba2a5e91500bd3079f
michaelmirhom <micharby73@gmail.com>
2023-04-29 19:45:40 -0500

    Another try

--- index.js
+++ index.js
@@ -1,1 +1,69 @@
 // Your code here
+function createEmployeeRecord([firstName, familyName, title, payPerHour]) {
+    return {
+      firstName,
+      familyName,
+      title,
+      payPerHour,
+      timeInEvents: [],
+      timeOutEvents: []
+    }
+  }
+  
+  function createEmployeeRecords(arr) {
+    return arr.map(createEmployeeRecord)
+  }
+  
+  function createTimeInEvent(employee, dateTime) {
+    const [date, hour] = dateTime.split(" ")
+  
+    employee.timeInEvents.push({
+      type: "TimeIn",
+      hour: parseInt(hour, 10),
+      date
+    })
+  
+    return employee
+  }
+  
+  function createTimeOutEvent(employee, dateTime) {
+    const [date, hour] = dateTime.split(" ")
+  
+    employee.timeOutEvents.push({
+      type: "TimeOut",
+      hour: parseInt(hour, 10),
+      date
+    })
+  
+    return employee
+  }
+  
+  function hoursWorkedOnDate(employee, date) {
+    const timeIn = employee.timeInEvents.find(event => event.date === date).hour
+    const timeOut = employee.timeOutEvents.find(event => event.date === date).hour
+  
+    return (timeOut - timeIn) / 100
+  }
+  
+  function wagesEarnedOnDate(employee, date) {
+    const hoursWorked = hoursWorkedOnDate(employee, date)
+  
+    return hoursWorked * employee.payPerHour
+  }
+  
+  function allWagesFor(employee) {
+    const datesWorked = employee.timeInEvents.map(event => event.date)
+  
+    return datesWorked.reduce((acc, date) => acc + wagesEarnedOnDate(employee, date), 0)
+  }
+  
+  function calculatePayroll(employees) {
+    return employees.reduce((acc, employee) => acc + allWagesFor(employee), 0)
+  }
+  
+  
+  
+  
+  
+  
+
